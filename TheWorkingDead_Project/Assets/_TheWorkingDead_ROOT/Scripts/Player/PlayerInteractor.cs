6b7f616bdbb0abee68d4da9ed64a75366af04c12
Thiago5l <santimarquesuni@gmail.com>
2025-10-14 12:47:42 +0200

    Player, arreglos de la grey y camera cinemachin

---
+++ TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Player/PlayerInteractor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    [Header("Respawn Configuration")]
+    [SerializeField] Transform respawnPoint; //Posición respawn
+    [SerializeField] float respawnFallLimit;//Limite en -y que de ser alcanzado respawn
+    Rigidbody playerRB;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.y <= respawnFallLimit) Respawn();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Respawn();
+        }
+    }
+    void Respawn()
+    {
+        playerRB.linearVelocity = new Vector3(0, 0, 0);
+        transform.position = respawnPoint.position;
+    }
+
+}
