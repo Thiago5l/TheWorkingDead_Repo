2eaa368664527b0fe47d3c9eb62fe2d167a9fbc5
MetalClanker <bhirosix@gmail.com>
2025-12-09 13:12:40 +0100

    Greg Sprite tweaks
    XD

--- TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/UI/Menus/GregIcon.cs
+++ TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/UI/Menus/GregIcon.cs
@@ -11,23 +11,34 @@ public class GregIcon : MonoBehaviour
 
     [SerializeField] Image actualGregSprite;
     [SerializeField] Sprite[] gregSprites;
+    [SerializeField] Transform gregImage;
 
     private void Update()
     {
-        if (zombieValue.Zombiedad == 0f && zombieValue.Zombiedad <= 0.200f)
+        if (zombieValue.Zombiedad >= 0f && zombieValue.Zombiedad <= 0.400f)
         {
             actualGregSprite.sprite = gregSprites[0];
             Debug.Log("Good");
         }
-        if (zombieValue.Zombiedad == 0.200f && zombieValue.Zombiedad <= 0.800f)
+        if (zombieValue.Zombiedad >= 0.400f && zombieValue.Zombiedad <= 0.850f)
         {
             actualGregSprite.sprite = gregSprites[1];
+            StartCoroutine(SpriteShake());
             Debug.Log("medium");
         }
-        if (zombieValue.Zombiedad == 0.800f && zombieValue.Zombiedad <= 100f)
+        if (zombieValue.Zombiedad >= 0.850f && zombieValue.Zombiedad <= 1.000f)
         {
             actualGregSprite.sprite = gregSprites[2];
             Debug.Log("Bad"); 
         }
+    }
+
+    IEnumerator SpriteShake()
+    {
+        gregImage.transform.localPosition = new Vector2(-59, 189);
+        yield return new WaitForSecondsRealtime(0.2f);
+        gregImage.transform.localPosition = new Vector2(-61, 193);
+        yield return new WaitForSecondsRealtime(0.2f);
+        gregImage.transform.localPosition = new Vector2(-60, 192);
     }
 }
