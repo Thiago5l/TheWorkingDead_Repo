abd37ce0e80901c93dd9aac7e34f270bce0e0d0b
Thiago5l <santimarquesuni@gmail.com>
2025-12-17 13:12:52 +0100

    continuación simon says

--- TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Interactables/SimonSays_Scripts/SaimonSaysTaskUI.cs
+++ TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Interactables/SimonSays_Scripts/SaimonSaysTaskUI.cs
@@ -1,23 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class SaimonSaysTaskUI : MonoBehaviour
 {
-    
-    
     [SerializeField] public GameObject uiTarea;
     [SerializeField] public GameObject player;
+
     [SerializeField] bool tareaAcabada;
     [SerializeField] bool playerCerca;
 
-
     [SerializeField] public List<Button> buttonsList = new List<Button>();
-  
-    [SerializeField] public float showTime;
-    [SerializeField] public float pauseTime;
-    [SerializeField] bool ifRobot;
-    [SerializeField] bool ifPlayer;
+    [SerializeField] public Color buttonColorSaved;
+
+    [SerializeField] float showTime = 0.5f;
+    [SerializeField] float pauseTime = 0.3f;
+
+    List<int> sequence = new List<int>();
+    int playerIndex;
+    bool playerTurn;
+
+    [SerializeField] int rondasCompletadas;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +29,7 @@ public class SaimonSaysTaskUI : MonoBehaviour
         uiTarea.SetActive(false);
         playerCerca = false;
         tareaAcabada = false;
+        rondasCompletadas = 0;
 
     }
 
@@ -47,6 +52,7 @@ public class SaimonSaysTaskUI : MonoBehaviour
 
     public void Interact()
     {
+        
         if (playerCerca && !tareaAcabada)
         {
             uiTarea.SetActive(true);
@@ -68,15 +74,71 @@ public class SaimonSaysTaskUI : MonoBehaviour
 
         for (int i = 0; i < buttonsList.Count; i++)
         {
-            //buttonsList[i].OnPointerClick += ButtonClicked;
+            int index = i;
+            buttonsList[i].onClick.AddListener(() => ButtonClicked(index));
+            StartGame();
         }
 
     }
 
-
-    void ButtonClicked()
+    void StartGame()
     {
+        sequence.Clear();
+        AddToSequence();
+    }
+    void AddToSequence()
+    {
+        playerTurn = false;
+        playerIndex = 0;
 
+        //int randomButton = Random.Range(0, buttonsList.Count);
+        //sequence.Add(randomButton);
+
+        //StartCoroutine(ShowSequence());
+    }
+
+    void ButtonClicked(int index)
+    {
+        if (!playerTurn) return;
+
+        if (index == sequence[playerIndex])
+        {
+            playerIndex++;
+
+            if (playerIndex >= sequence.Count)
+            {
+                StartCoroutine(NextRound());
+            }
+        }
+        else
+        {
+            GameOver();
+        }
+
+        
+    }
+    IEnumerator NextRound()
+    {
+        playerTurn = false;
+        rondasCompletadas += 1 ;
+        yield return new WaitForSeconds(1f);
+        AddToSequence();
+    }
+
+    void GameOver()
+    {
+        tareaAcabada = true;
+        playerTurn = false;
+    }
+    void HighlightButton(Button button)
+    {
+        buttonColorSaved = button.image.color;
+        button.image.color = Color.white;
+    }
+
+    void ResetButton(Button button)
+    {
+        button.image.color = buttonColorSaved;
     }
 
 }
