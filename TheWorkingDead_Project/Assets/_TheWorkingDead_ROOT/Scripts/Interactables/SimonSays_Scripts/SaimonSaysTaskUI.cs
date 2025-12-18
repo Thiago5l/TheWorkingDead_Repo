1f23263dd4a9d7441badfbc21c535b50de1e3247
Thiago5l <santimarquesuni@gmail.com>
2025-12-18 13:44:31 +0100

    cambios del simon dice desde 0

--- TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Interactables/SimonSays_Scripts/SaimonSaysTaskUI.cs
+++ TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Interactables/SimonSays_Scripts/SaimonSaysTaskUI.cs
@@ -1,8 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+[System.Serializable]
+public class SimonButton
+{
+    public Button button;
+    public Color normalColor;
+    public Color highlightColor;
+
+}
+
+
 public class SaimonSaysTaskUI : MonoBehaviour
 {
     [SerializeField] public GameObject uiTarea;
@@ -11,38 +21,43 @@ public class SaimonSaysTaskUI : MonoBehaviour
     [SerializeField] bool tareaAcabada;
     [SerializeField] bool playerCerca;
 
-    [SerializeField] public List<Button> buttonsList = new List<Button>();
-    [SerializeField] public Color buttonColorSaved;
+    [SerializeField] public List<SimonButton> buttonsList = new List<SimonButton>();
+    [SerializeField] public List<int> intOrdenBotonesPlayer = new List<int>();
+    //[SerializeField] public Color buttonColorSaved;
 
     [SerializeField] float showTime = 0.5f;
     [SerializeField] float pauseTime = 0.3f;
-
-    List<int> sequence = new List<int>();
+    
+    [SerializeField] List<int> sequence = new List<int>();
+    [SerializeField] int tamañoSequence;
     int playerIndex;
     bool playerTurn;
 
     [SerializeField] int rondasCompletadas;
+    [SerializeField] public int rondasACompletar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         uiTarea.SetActive(false);
         playerCerca = false;
         tareaAcabada = false;
+        
         rondasCompletadas = 0;
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("TaskPlayer"))
         {
             playerCerca = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("TaskPlayer"))
         {
             playerCerca = false;
         }
@@ -56,7 +71,8 @@ public class SaimonSaysTaskUI : MonoBehaviour
         if (playerCerca && !tareaAcabada)
         {
             uiTarea.SetActive(true);
-            StartCoroutine(WaitToStart());
+            InicioDeJuego();
+            //StartCoroutine(WaitToStart());
         }
         
     }
@@ -65,80 +81,199 @@ public class SaimonSaysTaskUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    IEnumerator WaitToStart()
-    {
-        yield return new WaitForSecondsRealtime(1);
-
-        for (int i = 0; i < buttonsList.Count; i++)
+        if(rondasCompletadas == rondasACompletar)
         {
-            int index = i;
-            buttonsList[i].onClick.AddListener(() => ButtonClicked(index));
-            StartGame();
+            StopAllCoroutines();
+            uiTarea.SetActive(false);
+            tareaAcabada = true;
         }
-
     }
 
-    void StartGame()
+
+    void InicioDeJuego()
     {
         sequence.Clear();
-        AddToSequence();
-    }
-    void AddToSequence()
-    {
-        playerTurn = false;
-        playerIndex = 0;
+        for (int i = 0; i <tamañoSequence; i++)
+        {
 
-        //int randomButton = Random.Range(0, buttonsList.Count);
-        //sequence.Add(randomButton);
+            int randomBtton = Random.Range(0, buttonsList.Count);
+            sequence.Add(randomBtton);
+        }
 
-        //StartCoroutine(ShowSequence());
     }
 
-    void ButtonClicked(int index)
+
+    public void PresionarBotonPlayer(int index)
     {
         if (!playerTurn) return;
-
-        if (index == sequence[playerIndex])
-        {
-            playerIndex++;
-
-            if (playerIndex >= sequence.Count)
-            {
-                StartCoroutine(NextRound());
-            }
-        }
         else
         {
-            GameOver();
+            intOrdenBotonesPlayer.Add(index);
         }
-
-        
-    }
-    IEnumerator NextRound()
-    {
-        playerTurn = false;
-        rondasCompletadas += 1 ;
-        yield return new WaitForSeconds(1f);
-        AddToSequence();
     }
 
-    void GameOver()
+    void SiguienteRonda()
     {
-        tareaAcabada = true;
-        playerTurn = false;
-    }
-    void HighlightButton(Button button)
-    {
-        buttonColorSaved = button.image.color;
-        button.image.color = Color.white;
+
     }
 
-    void ResetButton(Button button)
+
+
+
+
+
+
+
+
+    //void Start()
+    //{
+
+    //    uiTarea.SetActive(false);
+    //    playerCerca = false;
+    //    tareaAcabada = false;
+
+    //    rondasCompletadas = 0;
+
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("TaskPlayer"))
+    //    {
+    //        playerCerca = true;
+    //    }
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("TaskPlayer"))
+    //    {
+    //        playerCerca = false;
+    //    }
+    //}
+
+
+
+    //public void Interact()
+    //{
+
+    //    if (playerCerca && !tareaAcabada)
+    //    {
+    //        uiTarea.SetActive(true);
+    //        //StartCoroutine(WaitToStart());
+    //    }
+
+    //}
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (rondasCompletadas == rondasACompletar)
+    //    {
+    //        StopAllCoroutines();
+    //        uiTarea.SetActive(false);
+    //    }
+    //}
+
+    //IEnumerator WaitToStart()
+    //{
+    //    yield return new WaitForSecondsRealtime(1);
+
+
+    //    StartGame();
+
+    //    yield return new WaitForSecondsRealtime(1);
+    //    for (int i = 0; i < buttonsList.Count; i++)
+    //    {
+    //        int index = i;
+    //        buttonsList[i].button.onClick.AddListener(() => ButtonClicked(index));
+    //        buttonsList[i].button.image.color = buttonsList[i].normalColor;
+    //        StartGame();
+    //    }
+
+    //}
+
+    //void StartGame()
+    //{
+    //    sequence.Clear();
+    //    AddToSequence();
+    //}
+    //void AddToSequence()
+    //{
+    //    playerTurn = false;
+    //    playerIndex = 0;
+
+    //    int randomButton = Random.Range(0, (buttonsList.Count-1));
+    //    sequence.Add(randomButton);
+
+    //    StartCoroutine(ShowSequence());
+    //}
+
+    //IEnumerator ShowSequence()
+    //{
+    //    yield return new WaitForSeconds(0.5f);
+
+    //    for (int i = 0; i < sequence.Count; i++)
+    //    {
+    //        HighlightButton(/*buttonsList[index]*/sequence[i]);
+    //        yield return new WaitForSeconds(showTime);
+    //        ResetButton(sequence[i]);
+    //        yield return new WaitForSeconds(pauseTime);
+    //    }
+
+    //    foreach (int index in sequence)
+    //    {
+    //        HighlightButton(/*buttonsList[index]*/index);
+    //        yield return new WaitForSeconds(showTime);
+    //        ResetButton(/*buttonsList[index]*/index);
+    //        yield return new WaitForSeconds(pauseTime);
+    //    }
+
+    //    playerTurn = true;
+    //}
+    //public void ButtonClicked(int index)
+    //{
+
+    //    if (!playerTurn) Debug.Log("No es tu turno"); return;
+
+    //    if (index == sequence[playerIndex])
+    //    {
+    //        Debug.Log("has llegado al 1º if buttoncliked");
+
+    //        if (playerIndex >= sequence.Count)
+    //        {
+    //            Debug.Log("has llegado al 2ºº if buttoncliked");
+    //            StartCoroutine(NextRound());
+    //        }
+    //    }
+    //    else
+    //    {
+    //        GameOver();
+    //    }
+
+
+    //}
+    //IEnumerator NextRound()
+    //{
+    //    playerTurn = false;
+    //    rondasCompletadas += 1 ;
+    //    yield return new WaitForSeconds(1f);
+    //    AddToSequence();
+    //}
+
+    //void GameOver()
+    //{
+    //    tareaAcabada = true;
+    //    playerTurn = false;
+    //}
+    void HighlightButton(int index)
     {
-        button.image.color = buttonColorSaved;
+        buttonsList[index].button.image.color = buttonsList[index].highlightColor;
+    }
+
+    void ResetButton(int index)
+    {
+        buttonsList[index].button.image.color = buttonsList[index].normalColor;
     }
 
 }
