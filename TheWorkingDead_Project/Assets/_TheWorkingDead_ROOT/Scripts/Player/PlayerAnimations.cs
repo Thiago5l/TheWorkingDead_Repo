cc0cf429505c0ca60d8fd5b208f32b28c11f92db
AEttenn23 <andipl2323@gmail.com>
2026-02-02 10:05:32 +0100

    Fixing things

--- TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Player/PlayerAnimations.cs
+++ TheWorkingDead_Project/Assets/_TheWorkingDead_ROOT/Scripts/Player/PlayerAnimations.cs
@@ -6,13 +6,14 @@ public class PlayerAnimations : MonoBehaviour
     private Animator anim;
 
     [SerializeField] float velocidadUmbral = 0.1f;
-
+    [SerializeField] PlayerController playerController;
     private void Awake()
     {
         // Si el script está en un hijo, busca el Rigidbody en el padre
         rb = GetComponentInParent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
-
+        if (playerController==null)
+        playerController = GetComponentInChildren<PlayerController>();
     }
 
     private void Update()
@@ -25,10 +26,10 @@ public class PlayerAnimations : MonoBehaviour
 
         Vector3 velocidadCaminado = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
         float velocidad = velocidadCaminado.magnitude;
-        if (velocidad > velocidadUmbral && velocidad<2.1f)
+        if (playerController.isSprinting)
+            anim.SetFloat("Movimiento", 1f);
+        else if (velocidad >= 0.1)
             anim.SetFloat("Movimiento", 0.5f);
-        else if (velocidad >= 2.1f)
-            anim.SetFloat("Movimiento", 01f);
         else
             anim.SetFloat("Movimiento", 0f);
     }
